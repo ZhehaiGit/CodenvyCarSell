801e0eeead3deb0b1badd21b50c845fea84bec42
ZhehaiGit <zhehaizhu@gmail.com>
2017-05-25 19:22:04 +0000

    new login

--- src/main/java/edu/infsci2560/controllers/CustomerController.java
+++ src/main/java/edu/infsci2560/controllers/CustomerController.java
@@ -1,34 +1,34 @@
-package edu.infsci2560.controllers;
-
-import edu.infsci2560.models.Car;
-import edu.infsci2560.models.Customer;
-import edu.infsci2560.repositories.CustomerRepository;
-import javax.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-import org.springframework.web.servlet.view.RedirectView;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.filter.HiddenHttpMethodFilter;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-/**
- *
- * @author kolobj
- */
-@Controller
+//package edu.infsci2560.controllers;
+//
+//import edu.infsci2560.models.Car;
+//import edu.infsci2560.models.Customer;
+//import edu.infsci2560.repositories.CustomerRepository;
+//import javax.validation.Valid;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.HttpHeaders;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.validation.BindingResult;
+//import org.springframework.web.bind.annotation.ModelAttribute;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
+//import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+//import org.springframework.web.servlet.view.RedirectView;
+//import org.springframework.web.servlet.ModelAndView;
+//import org.springframework.web.filter.HiddenHttpMethodFilter;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+///**
+// *
+// * @author kolobj
+// */
+//@Controller
 //public class CustomerController {
 //    @Autowired
 //    private CustomerRepository CustRepository;
