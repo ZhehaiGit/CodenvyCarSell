22da88ec119145fd4ddaff0471b0f14dea3fd7cb
ZhehaiGit <zhehaizhu@gmail.com>
2017-05-25 20:49:18 +0000

    new login10

--- src/main/java/edu/infsci2560/repositories/CustomerRepository.java
+++ src/main/java/edu/infsci2560/repositories/CustomerRepository.java
@@ -1,18 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package edu.infsci2560.repositories;
-
-import edu.infsci2560.models.Customer;
-import java.util.List;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface CustomerRepository extends CrudRepository<Customer, Long> {
-    List<Customer> findByUsername(String username);
-//    public boolean contains(String userName) {
-//   
-//    }
-}
+///*
+// * To change this license header, choose License Headers in Project Properties.
+// * To change this template file, choose Tools | Templates
+// * and open the template in the editor.
+// */
+//package edu.infsci2560.repositories;
+//
+//import edu.infsci2560.models.Customer;
+//import java.util.List;
+//
+//import org.springframework.data.repository.CrudRepository;
+//
+//public interface CustomerRepository extends CrudRepository<Customer, Long> {
+//    List<Customer> findByUsername(String username);
+////    public boolean contains(String userName) {
+////   
+////    }
+//}
