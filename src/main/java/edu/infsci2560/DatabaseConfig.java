3afeffa586d282b2ab7f770c8ce2088038c758c2
ZhehaiGit <zhehaizhu@gmail.com>
2017-05-26 00:45:16 +0000

    aa

--- src/main/java/edu/infsci2560/DatabaseConfig.java
+++ src/main/java/edu/infsci2560/DatabaseConfig.java
@@ -33,9 +33,9 @@ public class DatabaseConfig {
         String password = dbUri.getUserInfo().split(":")[1];
         String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath() + "?sslmode=require";       
 
-        System.out.println(username);
-        System.out.println(password);
-        System.out.println(dbUrl);
+//        System.out.println(username);
+//        System.out.println(password);
+//        System.out.println(dbUrl);
 
         BasicDataSource basicDataSource = new BasicDataSource();
         basicDataSource.setUrl(dbUrl);
