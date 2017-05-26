45f6f3bf34b721b636a285f303f7a737b1513405
ZhehaiGit <zhehaizhu@gmail.com>
2017-05-26 21:00:14 +0000

    back

--- src/main/java/edu/infsci2560/repositories/CustomerRepository.java
+++ src/main/java/edu/infsci2560/repositories/CustomerRepository.java
@@ -11,7 +11,9 @@ import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 
 public interface CustomerRepository extends CrudRepository<Customer, Long> {
-    public List<Customer> findByLastName (String Username);
+    public List<Customer> findByUsername(String Username);
+    
+    public List<Customer> findByLastName(String Username);
 
 //      List<Customer> findByLastName(String lastName);
 
