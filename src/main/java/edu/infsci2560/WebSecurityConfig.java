4fc68d9abb1b733a93ee11646f209f1e692a36b6
ZhehaiGit <zhehaizhu@gmail.com>
2017-05-27 15:39:41 +0000

    sign up

--- src/main/java/edu/infsci2560/WebSecurityConfig.java
+++ src/main/java/edu/infsci2560/WebSecurityConfig.java
@@ -6,7 +6,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-
+import edu.infsci2560.CarAuthenticationProvider;
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
