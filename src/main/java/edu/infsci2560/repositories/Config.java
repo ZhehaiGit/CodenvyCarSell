179211d86cb971cfda0932bfafd62091839ab70e
ZhehaiGit <zhehaizhu@gmail.com>
2017-05-27 18:20:31 +0000

    query method

---
+++ src/main/java/edu/infsci2560/repositories/Config.java
@@ -0,0 +1,16 @@
+package edu.infsci2560.repositories;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.EntityManagerFactoryInfo;
+
+@Configurable
+@EnableJpaRepositories("com.acme.repositories")
+class ApplicationConfiguration {
+
+  @Bean
+  public EntityManagerFactoryInfo entityManagerFactory() {
+    return null;
+  }
+}
