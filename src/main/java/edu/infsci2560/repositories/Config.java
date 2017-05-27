a3f2fd305299ff16a6c4168cda4cc4b220230e39
ZhehaiGit <zhehaizhu@gmail.com>
2017-05-27 19:12:27 +0000

    query method config

--- src/main/java/edu/infsci2560/repositories/Config.java
+++ src/main/java/edu/infsci2560/repositories/Config.java
@@ -1,11 +1,54 @@
 package edu.infsci2560.repositories;
 
+import javax.persistence.EntityManagerFactory;
+
+import org.apache.tomcat.jdbc.pool.DataSource;
 import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.boot.autoconfigure.jdbc.EmbeddedDatabaseConnection;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.orm.jpa.EntityManagerFactoryInfo;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import com.google.gson.annotations.JsonAdapter;
+import com.google.gson.annotations.JsonAdapter;
 
 @Configurable
-@EnableJpaRepositories("com.acme.repositories")
+@ComponentScan
+@EnableJpaRepositories("net.ubilife.springjpa")
+
 class Config  {
+    @Bean
+    public DataSource dataSource() {
+        return (DataSource)new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2).addScript("schema.sql").build();
+    }
+    @Bean
+    public JsonAdapter jpaAdapter() {
+        return (JsonAdapter)new HibernateJpaVendorAdapter();    
+    }
+    
+    @Bean 
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource ds, JpaVendorAdapter jva) {
+        LocalContainerEntityManagerFactoryBean emf = new LocalContainerEntityManagerFactoryBean();
+        emf.setDataSource(ds);
+        emf.setJpaVendorAdapter(jva);
+        emf.setPackagesToScan(new String[]{"net.ubilife.springjpa"});
+        
+        return emf;
+    }
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(emf);
+        
+        return transactionManager;
+    }
+    
 }
