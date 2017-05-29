80a0e101bf11911901222cd99b6dfa0e805c49ac
ZhehaiGit <zhehaizhu@gmail.com>
2017-05-29 17:59:40 +0000

    logIn

--- src/main/java/edu/infsci2560/WebSecurityConfig.java
+++ src/main/java/edu/infsci2560/WebSecurityConfig.java
@@ -19,7 +19,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
-                .antMatchers("/", "/home","/signup", "/public/*").permitAll()
+                .antMatchers("/", "/cars","/signup", "/public/*").permitAll()
 //                .antMatchers("/","/public/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
