15435d334687228ab56dcd26b114d883862c207b
ZhehaiGit <zhehaizhu@gmail.com>
2017-05-30 16:59:04 +0000

    view customer

--- src/main/java/edu/infsci2560/controllers/CustomerController.java
+++ src/main/java/edu/infsci2560/controllers/CustomerController.java
@@ -24,6 +24,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import edu.infsci2560.repositories.CarRepository;
 /**
  *
  * @author kolobj
