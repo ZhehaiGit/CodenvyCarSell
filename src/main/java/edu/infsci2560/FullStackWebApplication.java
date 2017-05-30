9bd6c1b21a85cccf7d528a0cb03595e3c0f2fd66
ZhehaiGit <zhehaizhu@gmail.com>
2017-05-30 15:26:24 +0000

    findByUsername

--- src/main/java/edu/infsci2560/FullStackWebApplication.java
+++ src/main/java/edu/infsci2560/FullStackWebApplication.java
@@ -12,6 +12,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import java.util.List;
 
 @SpringBootApplication
 public class FullStackWebApplication {
