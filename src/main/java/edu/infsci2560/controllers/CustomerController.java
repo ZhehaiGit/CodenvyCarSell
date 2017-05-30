7900a5cda6aa96af89d729c0346e8107a8b1363a
ZhehaiGit <zhehaizhu@gmail.com>
2017-05-30 21:52:57 +0000

    aa

--- src/main/java/edu/infsci2560/controllers/CustomerController.java
+++ src/main/java/edu/infsci2560/controllers/CustomerController.java
@@ -103,6 +103,7 @@ public class CustomerController {
         List<Car> cars = customer.getCars();
         cars.add(car);
         customer.setCars(cars);
+        CustRepository.save(customer);
         return new ModelAndView(new RedirectView("sellcars"));
 //        return new ModelAndView("sellcars", "cars", repository.findAll());
     }
