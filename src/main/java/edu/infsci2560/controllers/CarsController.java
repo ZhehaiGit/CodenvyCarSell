8cea3260da65118b7e0c42de2f43b7c932c20665
ZhehaiGit <zhehaizhu@gmail.com>
2017-05-30 15:49:20 +0000

    list car

--- src/main/java/edu/infsci2560/controllers/CarsController.java
+++ src/main/java/edu/infsci2560/controllers/CarsController.java
@@ -76,9 +76,9 @@ public class CarsController {
     @RequestMapping(value = "cars", method = RequestMethod.GET)
     public ModelAndView cars() {
         
-//        List<Car> cars = cstmRepository.findByUsername(request.getRemoteUser()).get(0).getCars();
+        List<Car> cars = cstmRepository.findByUsername(request.getRemoteUser()).get(0).getCars();
 //        return new ModelAndView("cars", "cars", cars);
-          return new ModelAndView("cars", "cars", repository.findAll());  
+//          return new ModelAndView("cars", "cars", repository.findAll());  
     }
     
     @RequestMapping(value = "sellcars", method = RequestMethod.GET)
