24426937b7e313c724b74f121ae4ea8e651a3776
ZhehaiGit <zhehaizhu@gmail.com>
2017-05-30 17:30:00 +0000

    view customer

--- src/main/java/edu/infsci2560/controllers/CustomerController.java
+++ src/main/java/edu/infsci2560/controllers/CustomerController.java
@@ -34,7 +34,6 @@ public class CustomerController {
     @Autowired
 	HttpServletRequest request;
     @Autowired
-//    private CarRepository repository;
     private CustomerRepository CustRepository;
     
 //    private String Uname = request.getRemoteUser();
