cb0ae78a7e452a8bde22362c6ca8a63ad89b716d
ZhehaiGit <zhehaizhu@gmail.com>
2017-05-31 20:56:04 +0000

    aa

--- src/main/java/edu/infsci2560/controllers/CustomerController.java
+++ src/main/java/edu/infsci2560/controllers/CustomerController.java
@@ -127,7 +127,7 @@ public class CustomerController {
            List<Car> cars = customer.getCars();
            for (int i = 0; i < cars.size(); i++) {
                Car c = cars.get(i);
-               if (c.get(id) == id) {
+               if (c.getId(id) == id) {
                    cars.remove(i);
                    break;
                }
