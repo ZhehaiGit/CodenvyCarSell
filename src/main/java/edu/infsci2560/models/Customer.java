c57bce4a817b859c52d736bef2024386885134ec
ZhehaiGit <zhehaizhu@gmail.com>
2017-05-31 18:51:31 +0000

    aa

--- src/main/java/edu/infsci2560/models/Customer.java
+++ src/main/java/edu/infsci2560/models/Customer.java
@@ -26,7 +26,7 @@ public class Customer {
     private String password;
     private String email;
     //(cascade = {CascadeType.ALL})
-    @OneToMany(cascade = {CascadeType.ALL}, mappedBy ="order")
+    @OneToMany(cascade = {CascadeType.ALL})
     private List<Car> cars;
 
     protected Customer() {
