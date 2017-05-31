77132b2d1ca05632ad9ff6acc1aea1b44d486bd3
ZhehaiGit <zhehaizhu@gmail.com>
2017-05-31 17:20:53 +0000

    cascade

--- src/main/java/edu/infsci2560/controllers/CustomerController.java
+++ src/main/java/edu/infsci2560/controllers/CustomerController.java
@@ -110,7 +110,7 @@ public class CustomerController {
         List<Car> cars = customer.getCars();
         cars.add(car);
         customer.setCars(cars);
-        session.save(cars);
+        Session.save(cars);
         CustRepository.save(customer);
 //        return new ModelAndView(new RedirectView("sellcars"));
         return new ModelAndView("sellcars","customer",CustRepository.findByUserName(Uname).get(0));
