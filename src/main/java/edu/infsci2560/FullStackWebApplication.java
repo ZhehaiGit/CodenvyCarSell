03079ef790c436e4410a120cb2850808dff1e0df
ZhehaiGit <zhehaizhu@gmail.com>
2017-08-02 18:54:41 +0000

    datatable

--- src/main/java/edu/infsci2560/FullStackWebApplication.java
+++ src/main/java/edu/infsci2560/FullStackWebApplication.java
@@ -41,9 +41,9 @@ public class FullStackWebApplication {
         repository.save(new Car(7L, "DodRam", WorkoutType.Van, "Dodgesfs", "wdy@ppp.com", "wyj"));
         repository.save(new Car(8L, "DodRam", WorkoutType.Van, "Dodgesfs", "wdy@ppp.com", "wyj"));
         repository.save(new Car(9L, "DodRam", WorkoutType.Van, "Dodgesfs", "wdy@ppp.com", "wyj"));
-        repository.save(new Car(9L++, "DodRam", WorkoutType.Van, "Dodgesfs", "wdy@ppp.com", "wyj"));
-        repository.save(new Car(9L+2, "DodRam", WorkoutType.Van, "Dodgesfs", "wdy@ppp.com", "wyj"));
-        repository.save(new Car(9L+3, "DodRam", WorkoutType.Van, "Dodgesfs", "wdy@ppp.com", "wyj"));
+        repository.save(new Car(10L, "DodRam", WorkoutType.Van, "Dodgesfs", "wdy@ppp.com", "wyj"));
+        repository.save(new Car(11L, "DodRam", WorkoutType.Van, "Dodgesfs", "wdy@ppp.com", "wyj"));
+        repository.save(new Car(12L, "DodRam", WorkoutType.Van, "Dodgesfs", "wdy@ppp.com", "wyj"));
         
         
         
